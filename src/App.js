1d3bf397c50ba3435844939559dc26ddc3766e43
Artem Guzov <guzovartem@yandex.ru>
2021-04-06 19:02:34 +0300

    Make a bit front

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
-import { listNotes } from './graphql/queries';
-import { createNote as createNoteMutation, deleteNote as deleteNoteMutation } from './graphql/mutations';
 import Amplify, { API } from 'aws-amplify';
 import awsconfig from './aws-exports';
 
 Amplify.configure(awsconfig);
 
-const initialFormState = { name: '', description: '' }
-const apiName = 'newRest';
-const path = '/path'; 
+const initialFormState = { first: '', second: '' }
+const apiName = 'APIDB';
+const path = '/api'; 
 const myInit = { // OPTIONAL
     headers: {}, // OPTIONAL
     response: true, // OPTIONAL (return the entire Axios response object instead of only response.data)
@@ -37,21 +35,46 @@ function App() {
 
   }
 
-  /*async function createNote() {
-    if (!formData.name || !formData.description) return;
-    await API.graphql({ query: createNoteMutation, variables: { input: formData } });
-    setNotes([ ...notes, formData ]);
-    setFormData(initialFormState);
-  }*/
+  async function createNote() {
+	  var first = document.getElementById('inputName').value;
+	  var second = document.getElementById('inputDescription').value;
+	  const params = {
+    body: {
+        first: first,
+        second: second
+    },
+};
+    API.put(apiName, path, params)
+  .catch(error => {
+    console.log(error.response);
+  });
+  }
+  
+  async function deleteNote() {
+	  var val = document.getElementById('inputName').value;
+    API.del(apiName, path + '/object/' + val , {})
+  .catch(error => {
+    console.log(error.response);
+  });
+  }
 
-  /*async function deleteNote({ id }) {
-    const newNotesArray = notes.filter(note => note.id !== id);
-    setNotes(newNotesArray);
-    await API.graphql({ query: deleteNoteMutation, variables: { input: { id } }});
-  }*/
 
   return (
+   <div className="App">
               <button onClick={fetchNotes}>fetchNotes</button>
+			<input id ="inputName"
+        onChange={e => setFormData({ ...formData, 'name': e.target.value})}
+        placeholder="Note name"
+        value={formData.name}
+      />
+      <input id = "inputDescription"
+        onChange={e => setFormData({ ...formData, 'description': e.target.value})}
+        placeholder="Note description"
+        value={formData.description}
+      />
+	   <button onClick={createNote}>Create Note</button>
+	   <button onClick={deleteNote}>deleteNote</button>
+	  </div>
   );
 }
 
